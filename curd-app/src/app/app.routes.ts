29ca585fc3103c5312120be3b1ad19681e3fdee9
Lavithesh <157971584+lavithesh@users.noreply.github.com>
2026-04-23 23:52:38 +0530

    post component created

--- curd-app/src/app/app.routes.ts
+++ curd-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Index } from './post/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"post",
+        component: Index
+    }
+];
